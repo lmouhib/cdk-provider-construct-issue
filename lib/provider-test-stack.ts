5171fba30fe0b99b8370d5bb39bf95850af694f6
Lotfi Mouhib <mouhib@amazon.com>
2022-05-20 11:39:18 +0100

    initial commit

--- lib/provider-test-stack.ts
+++ lib/provider-test-stack.ts
@@ -1,16 +1,46 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Aws } from 'aws-cdk-lib';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Provider } from 'aws-cdk-lib/custom-resources';
+import { Effect, Role, ManagedPolicy, ServicePrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
+
 
 export class ProviderTestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProviderTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const providerManagedPolicy = new ManagedPolicy(this, 'providerManagedPolicy', {
+      statements: [new PolicyStatement({
+        actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
+        resources: [`arn:aws:logs:${Aws.REGION}:${Aws.ACCOUNT_ID}:*`],
+        effect: Effect.ALLOW,
+      })],
+    });
+
+    const providerRole = new Role(this, 'providerRole', {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+      managedPolicies: [providerManagedPolicy],
+    });
+
+    const onEvent : Function = new Function (this, 'onEvent', {
+      runtime: Runtime.PYTHON_3_8,
+      code: Code.fromAsset(path.join(__dirname)),
+      handler: 'lambda.on_event',
+    });
+
+    const isComplete : Function = new Function (this, 'isComplete', {
+      runtime: Runtime.PYTHON_3_8,
+      code: Code.fromAsset(path.join(__dirname)),
+      handler: 'lambda.is_complete',
+    });
+
+    new Provider(this, 'customresourceprovider', {
+      onEventHandler: onEvent,
+      isCompleteHandler: isComplete,
+      role: providerRole,
+    });
+
   }
 }
